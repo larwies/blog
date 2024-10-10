3008858894a4bf7df78bb84925109fe2f436a532
larwies <farquetelarissa@gmail.com>
2024-10-10 11:17:02 -0300

    a

--- blog-main/includes/busca.php
+++ blog-main/includes/busca.php
@@ -1,4 +1,6 @@
-<form action="" method="get" class="form-inline my-2 my-lg-0">
-    <input type="search" name="busca" class="form-control mr-sm-2" placeholder="Busca" aria-label="Busca">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+<form class="form-inline my-2 my-lg-0" method='get' action=''>
+    <input class="form-control mr-sm-2" type="search" name='busca'
+        placeholder="Busca" aria-label="Busca">
+    <button class="btn btn-outline-success my-2 my-sm-0"
+        type="submit">Buscar</button>
 </form>
